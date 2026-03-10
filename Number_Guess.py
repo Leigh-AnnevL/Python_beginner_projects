123977990e02cb3dd919d224e038fcdae75fa0af
Leigh-Anne <annievl313@icloud.com>
2026-03-10 16:44:54 +0200

    Change target number and enhance user prompts
    Updated number to 2000 and improved guessing feedback.

--- Number_Guess.py
+++ Number_Guess.py
@@ -1,7 +1,10 @@
-number = 1234
+number = 2000
 user_guess = int(input("Guess the random number: "))
 
 while user_guess != number:
-    user_guess = (int(input("Wrong Answer, try again!: ")))
-
+    if user_guess > number:
+     user_guess = (int(input("Too High! Try again: ")))
+    elif user_guess < number:
+       user_guess = (int(input("Too Low! Try Again: ")))
+       
 print("Success")
